53cca12f51ba40b26492ca7982691a1395d930ca
smarten <smarten.developer@gmail.com>
2020-11-19 22:28:30 -0800

    portfolio

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,30 +1,47 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+    backgroundColor: "#302E2E",
+    color: "white",
+    textAlign: "center",
+  },
+  media: {
+    height: "250px",
+    width: "100%",
+  },
+}));
 
 export default function PortfolioCard(props) {
+  const classes = useStyles();
   return (
-    <Card style={{width: "50%"}}>
-      <CardActionArea>
-        <CardMedia
-          image={props.image}
-        ><img src={props.image}/></CardMedia>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.title}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  )
+    <a style={{ textDecoration: "none" }} href={props.link}>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            component="img"
+            alt="Day Planner"
+            image={props.img}
+            title="Day planner"
+          />
+          <hr
+            style={{ border: "none", backgroundColor: "black", height: "5px" }}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="h2">
+              {props.title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </a>
+  );
 }
