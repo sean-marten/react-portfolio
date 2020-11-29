dde7405521e13f9b202c9a52d57075407ae938bc
smarten <smarten.developer@gmail.com>
2020-11-28 21:14:35 -0800

    hash

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Biography from './components/Biography';
-import Navbar from './components/Navbar';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact'
-
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Biography from "./components/Biography";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
-
   return (
     <Router>
-      <Navbar/>
-      <Route path="/contact" component={Contact}/>
-      <Route path="/portfolio" component={Portfolio}/>
-      <Route exact path="/" component={Biography}/>
+      <Navbar />
+      <Route path="/contact" component={Contact} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route exact path="/" component={Biography} />
     </Router>
   );
 }
